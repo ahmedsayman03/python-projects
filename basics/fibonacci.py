bd91997df69cd1b476ce514914081be428434c2c
ahmedsayman03 <ahmed.s.ayman16@gmail.com>
2022-10-04 15:12:44 -0400

    Follow Pep 8

--- basics/fibonacci.py
+++ basics/fibonacci.py
@@ -1,27 +1,18 @@
 def fib():
-    '''
-    The fibonacci sequence is such that each new number
-    is the sum of the two previous numbers.
-    The first two numbers are 0 and 1.
-    
-    e.g. 0, 1, 1, 2, 3, 5, 8, 13
-    '''
-    x = 0
-    y = 1
-    i = 0 # incrementing counter
-    
-    print(x, end = ' ')
-    print(y, end = ' ')
-    while i < 10: # 10 fib numbers after 0 and 1
-        n = x + y
-        
-        print(n, end = ' ')
+    """Prints fibonacci sequence: Each number is the sum of the two previous numbers. Starting with 0 and 1.
 
-        x = y 
-        y = n
-        i += 1
+    e.g.
+    0, 1, 1, 2, 3, 5, 8, 13, 21, 34
+    """
+
+    f = 0
+    s = 1
+
+    for i in range(10):
+        print(f, end=' ')
+        n = f + s
+        f = s
+        s = n
+
 
 fib()
-        
-        
-    
--- basics/finding_even_odd_continue.py
+++ basics/finding_even_odd_continue.py
@@ -1,15 +1,14 @@
 
 def finding_even_odd_continue():
-    '''
-    numbers from 1 to 10, not including 10, even or odd?
+    """Prints if first 10 numbers are even or odd. Uses continue."""
 
-    uses continue
-    '''
-    for i in range(1, 10):
+    for i in range (1, 10):
         if i % 2 == 0:
-            print(i, 'even')
+            print(i, 'Even')
             continue
-        # could remove the continue above and put else here
-        print(i, 'odd')
+
+        #  Could remove continue and replace with an else here
+        print(i, 'Odd')
+
 
 finding_even_odd_continue()
