0c618508b44a545aadd2eaab1854a99d236de046
ahmedsayman03 <ahmed.s.ayman16@gmail.com>
2022-10-05 12:01:55 -0400

    Modify for PEP 8

--- basics/unpack_args.py
+++ basics/unpack_args.py
@@ -1,15 +1,15 @@
 def unpack():
-    listy = [1,2,3,4,5]
+    """Unpacks list into string and dict into keys (and key value)."""
+    listy = [1, 2, 3, 4, 5]
     print(*listy)
+    # makes list a string, divided by spaces
+    # prints
+    # 1 2 3 4 5
 
-    # makes list a string
 
     dicty = {'a':'hi', 'b':'bye'}
     print(*dicty)
-
     # prints keys, but not values
+
     # **dicty
     # is key and value, but hard to print
-    
-    
-    
