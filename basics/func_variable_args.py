1882a4447c51896fe3c4ac9a2c0fbac8f1d61c6e
ahmedsayman03 <ahmed.s.ayman16@gmail.com>
2022-10-05 11:31:20 -0400

    Modify for PEP 8

--- basics/func_variable_args.py
+++ basics/func_variable_args.py
@@ -3,6 +3,21 @@
 # with different argument numbers
 
 def ask_ok(prompt, retries=4, reminder='Please try again!'):
+    """Demonstrates calling function in multiple different ways.
+
+    e.g. Can be called four ways
+    ask_ok('?')
+
+    ask_ok('?', 2)
+
+    ask_ok('?', 2, 'Come again?')
+
+    ask_ok('?', reminder='Come again?', retries=2)
+
+    ask_ok(reminder='Come again?', retries=2, '?')
+        Raises Error
+        Positional arguments cannot come after kwargs.
+    """
     while True:
         ok = input(prompt)
         if ok in ('y', 'ye', 'yes'):
@@ -15,42 +30,31 @@ def ask_ok(prompt, retries=4, reminder='Please try again!'):
         print(reminder)
 
 
-# function above can be called three ways:
-
-# ask_ok('Do you want to quit?')
-
-# ask_ok('Do you want to quit?', 2)
-
-# ask_ok('Do you want to quit?', 2, 'Incorrect input, try again')
-
-'''
-Note: default value only assigned once.
-
-def f(a, L=[]):
-    L.append(a)
-    print(L)
-    return L
-
-f(1)
-f(2)
-f(3)
-
-L is
-[1, 2, 3]
-
-If you want default value to change:
-def f(a, L=None):
-    if L is None:
-        L = []
-    L.append(a)
-    print(L)
-    return L
-
-f(1)
-f(2)
-f(3)
-
-L is
-[3]
-'''
-
+# Note: default value only assigned once.
+#
+# def f(a, L=[]):
+#     L.append(a)
+#     print(L)
+#     return L
+#
+# f(1)
+# f(2)
+# f(3)
+#
+# L is
+# [1, 2, 3]
+#
+# If you want default value to change:
+# def f(a, L=None):
+#     if L is None:
+#         L = []
+#     L.append(a)
+#     print(L)
+#     return L
+#
+# f(1)
+# f(2)
+# f(3)
+#
+# L is
+# [3]
