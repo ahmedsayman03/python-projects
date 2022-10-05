417b7283e3d19f1b1f5379879ab873b22c60ac18
ahmedsayman03 <ahmed.s.ayman16@gmail.com>
2022-10-05 11:23:53 -0400

    Modify for PEP 8

--- basics/func_kwargs_call.py
+++ basics/func_kwargs_call.py
@@ -1,29 +1,36 @@
-def parrot(voltage, state='state', action='action', type='type'):
-    print(voltage)
+def parrot(first, state='state', action='action', type='type'):
+    """Demonstrates when to use kwargs and position arguments at the same time.
+
+    e.g.
+    parrot()
+        Raises Error
+        Argument first is required.
+
+    parrot(1, state='2')
+
+    parrot(first=1, 's')
+        Raises Error
+        Positional argument cannot come after kwarg.
+
+    parrot(guy=1)
+        Raises Error
+        No such argument.
+
+    parrot(1, 's')
+        Without kwargs ok since positional is provided.
+
+    parrot(1, state='s')
+        With kwargs ok since positional is provided.
+
+    parrot(first=1, state='s')
+
+    parrot(state='s', first=1)
+
+    parrot(first=1, 1)
+    """
+    print(first)
     print(state)
     print(action)
     print(type)
-
-
-parrot(1)
-
-
-parrot(1, 's')
-
-parrot(1, state='s')
-
-parrot(voltage=1, state='s')
-
-parrot(state='s', voltage=1)
-
-'''
-This returns error:
-parrot() - 1 argument (voltage) required
-
-parrot(voltage=1, 's') - non-kwarg cannot come after kwarg
-
-parrot(1, voltage=2) - gave an argument twice
-
-parrot(guy='me') - no such argument exists
 
 
