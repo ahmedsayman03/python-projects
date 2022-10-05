f7c4979db9f86cf4a8df14dd5526601802ab32b1
ahmedsayman03 <ahmed.s.ayman16@gmail.com>
2022-10-05 11:57:09 -0400

    Modify for PEP 8

--- basics/modify_dict.py
+++ basics/modify_dict.py
@@ -1,23 +1,32 @@
 def del_inactive():
+    """Deletes keys in dictionary if the value is 'inactive'.
+
+    Demonstrates that you cannot delete from dictionay you are iterating over.
+    Make a copy to iterate over instead."""
+
     users = {'Hans': 'active', 'Éléonore': 'inactive', '景太郎': 'active'}
     # print("items: ", users.copy().items())
     # above line shows key, value
     # so to loop over it:
     # for key, value in users.copy().items()
+
     for i in users.copy():
+        # i is key
         print(i)
+        # users[i] is value
         print(users[i])
+
         if users[i] == 'inactive':
             users.pop(i)
     print(users)
 
 
-
 def add_active():
-    '''
-    Does what the above code does, but instead creates
-    new dictionary and adds active keys to it
-    '''
+    """Returns a dictionary of all active keys in dictionary.
+
+    Again, don't modify dictionary you are iterating over.
+    Create a new one and add to it."""
+
     users = {'Hans': 'active', 'Éléonore': 'inactive', '景太郎': 'active'}
     active_users = {}
     for i in users:
@@ -27,4 +36,3 @@ def add_active():
 
     print(active_users)
 
-add_active()
