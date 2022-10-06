98e40d467dc2473476ac839096377d1b31ac4f8e
ahmedsayman03 <ahmed.s.ayman16@gmail.com>
2022-10-06 11:22:38 -0400

    Modify for PEP 8

--- data-structures/list_nested_comprehensions.py
+++ data-structures/list_nested_comprehensions.py
@@ -1,130 +1,40 @@
-'''
-Working with nested comprehensions.
-'''
-
 def m_t():
-    matrix = [
-        [1, 2, 3, 4],
-        [5, 6, 7, 8],
-        [9, 10, 11, 12],
-        ]
+    """Prints transpose of matrix.
 
-    trans = [ [row[i] for row in matrix] for i in range(4) ]
-    print(trans)
+    Demonstrates using lists within lists (i.e. lists within list comprehensions)
 
-m_t()
-
-
-
-def matrix_transpose():
-    '''
-    Takes a matrix, transposes it.
     e.g.
-
     matrix = [
-        [1, 2, 3, 4],
-        [5, 6, 7, 8],
-        [9, 10, 11, 12],
-        ]
+    [1, 2, 3, 4],
+    [5, 6, 7, 8],
+    [9, 10, 11, 12],
+    ]
 
-    trans = [
+        prints
+        [
         [1, 5, 9],
         [2, 6, 10],
-        [3, 7,11],
-        [4, 8, 12]
+        [3, 7, 11],
+        [4, 8, 12],
         ]
-    '''
-    
+
+
+    """
     matrix = [
         [1, 2, 3, 4],
         [5, 6, 7, 8],
         [9, 10, 11, 12],
         ]
 
-    trans = []
+    # Do [] 4 times
+    n = [[] for i in range(4)]
+
     for i in range(4):
-        trans.append([])
+        for row in matrix:
+            n[i].append(row[i])
+    print(n)
 
-    # OR
-    # [ trans.append([]) for i in range(4) ]
-    # matrix_creation() does the above
-        
-    ind = 0
-    for row in matrix: # [1234] [5678] [9 10 11 12]
-        for n in row: # 1234
-            # all i'm doing here is trans[i].append(row[i])
-            # where i = 0-3
-            trans[ind].append(n)
-            ind += 1
-        ind = 0
-        
-    print(trans)
-    '''
-    This is easier:
-    for row in matrix:
-        for i in range(4):
-            trans[i].append(row[i])
-
-    efficient_mat_tra() does the above
-    '''
-
-    '''
-    To do the above, but with comprehension:
-    trans = [ [row[i] for row in matrix] for i in range(4)]
-    efficient_mat_tra() does the above
-    '''
-    
-
-    
-
-    
-def matrix_creation():
-    '''Create [ [], [], [], [] ]
-    '''
-    mat = []
-    [ mat.append([]) for i in range(4) ]
-    print(mat)
-
-    # [ [], [], [], [] ]
-
-def efficient_mat_tra():
-    matrix = [
-        [1, 2, 3, 4],
-        [5, 6, 7, 8],
-        [9, 10, 11, 12],
-        ]
-    trans = [[], [], [], []]
-    
-    for row in matrix: # [1234] [5678] [9 10 11 12]
-        # [1234]
-        for i in range(4):
-            trans[i].append(row[i])
-        
-    print('reg', trans)
-
-    com_trans = [ [row[i] for row in matrix] for i in range(4)]
-    print('com', com_trans)
-    
-            
-        
-        
-    
-def test():
-    matrix = [
-        [1, 2, 3, 4],
-        [5, 6, 7, 8],
-        [9, 10, 11, 12],
-        ]
-
-    #trans = []
-    #trans.append([row[0] for row in matrix])
-    #trans.append([row[1] for row in matrix])
-    #trans.append([row[i] for row in matrix] for i in range(4))
-    '''
-    for i in range(4):
-        trans.append([row[i] for row in matrix])
-    '''
-    trans = [ [row[i] for row in matrix] for i in range(4) ]
-    print(trans)
-
-
+    # If you have trouble doing comprehensions, do it the regular way first.
+    # then go from there.
+    t = [[row[i] for row in matrix] for i in range(4)]
+    print(t)
