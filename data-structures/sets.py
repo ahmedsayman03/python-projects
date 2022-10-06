d9910b0a6ecb6d7ece91ac5d6071f6d4afe61f94
ahmedsayman03 <ahmed.s.ayman16@gmail.com>
2022-10-06 11:50:21 -0400

    Modify for PEP  8

--- data-structures/sets.py
+++ data-structures/sets.py
@@ -1,23 +1,28 @@
-'''sets'''
-
 def sets():
-    # sets are not ordered!
-    a = set('abcdefaa')
-    b = set('ghiabj')
-    
+    """Prints the various math operations for sets (Similar to sets and collections learned in algebra).
+
+    e.g.
+    a = {'123'}
+    b = {'345'}
+
+    Sets can show what's in both a and b, what's shared, what's not shared.
+
+    Note: Sets are not ordered."""
+
+    a = set('abcdefaah')
+    b = set('abcdefddg')
     
     print(a)
     print(b)
-    print(a-b) # in a, not in b
-    print(a|b) # in a, in b, in both
-    print(a&b) # both in a and b
-    print(a^b) # in a, in b, not in both
+    print(a - b)  # a not b,      in a, not in b
+    print(a | b)  # all,          in a, in b, in both
+    print(a & b)  # shared,       both in a and b
+    print(a ^ b)  # not shared,   in a, in b, not in both
 
 
-
-# sets have comprehensions too like lists
 def set_com():
+    """Prints set comprehension."""
     a = {x for x in 'abcd' if x not in 'ab'}
     print(a)
 
-set_com()
+
