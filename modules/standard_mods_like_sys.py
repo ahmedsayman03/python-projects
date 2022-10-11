f0fade768649e3be91adfde315cbdfc230ebf493
ahmedsayman03 <ahmed.s.ayman16@gmail.com>
2022-10-11 10:48:09 -0400

    Add doc explaining python standard modules like sys

---
+++ modules/standard_mods_like_sys.py
@@ -0,0 +1,24 @@
+def std_mods():
+    """Explains standard modules in Python Library Reference (such as sys, not core to language but built in)."""
+
+    # Python has a library of standard modules called Python Library Reference
+
+    # Some modules are built into the interpreter (operations like sys calls)
+    # Not core to the language, but built it
+
+    # E.g. sys module
+    # sys.ps1
+    # sys.ps2
+    # Are the prompts. These variables are available in interactive mode. They are the following, respectively:
+    # >>>
+    # ...
+
+    # The variable
+    # sys.path
+    # is a list of strings. Interpreter searches path for modules.
+    # Taken from environment variables PYTHONPATH
+
+    # Can be changed like so:
+    # import sys
+    # sys.path.append('/a/b/c')
+    
