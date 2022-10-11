11376890bc418fbe40bd26c27e970e066b9ddbb8
ahmedsayman03 <ahmed.s.ayman16@gmail.com>
2022-10-11 10:20:41 -0400

    Add doc on how python searches for modules

---
+++ modules/search_path.py
@@ -0,0 +1,11 @@
+def search_path():
+    """Explains how python searches for modules when importing them."""
+
+    # import spam
+    # Python checks
+    # sys.path
+
+    # sys.path is initialized in cwd or the input script directory (script being run)
+    # OR
+    # PYTHONPATH
+    
