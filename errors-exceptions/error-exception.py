aac53aa664c0adb3431c99575b4180b2acb23969
ahmedsayman03 <ahmed.s.ayman16@gmail.com>
2022-10-12 11:05:08 -0400

    Add doc on error handling

---
+++ errors-exceptions/error-exception.py
@@ -0,0 +1,24 @@
+def error():
+    """Syntax / Parsing errors (statement not syntactically correct."""
+
+    # for i in range(4)
+    #    print('hello')
+
+    # Semi-colon missing
+    # Not a correct statement syntactically
+
+    pass
+
+
+def exception():
+    """Syntax correct but still an error (exception). Errors during execution.
+
+    e.g.
+    Name not defined (no var)
+
+    Cannot divide by zero
+
+    '2' + 2
+    Type error"""
+
+    pass
