a0338956b9136f58d61119bf36d2ac5f9e7b3efa
ahmedsayman03 <ahmed.s.ayman16@gmail.com>
2022-10-12 10:36:41 -0400

    Add doc on json (de)serialization

---
+++ input-output/json-serialize-de.py
@@ -0,0 +1,25 @@
+def json():
+    """JSON (JavaScript Object Notation) (data interchange format) serialization."""
+
+    # reading / writing strings, easy
+    # with numbers, you have to convert to int() when reading, convert to str() when writing
+    # complicated, especially when working with complex data types like dicts or nested lists
+
+    # json - standard module
+    # converts python data-types to strings (serialization)
+    # string representation to data-type (deserialization)
+
+    import json
+    print(json.dumps([1, 'simple', 'list']))
+
+    # json.dump(x, f)
+    # dump serialized string x into text file open for writing f
+
+    # to decode
+    # x = json.load(f)
+    # f is a text-file object
+
+    
+
+json()
+
