21642a59b8d6c33e7b4589e191300f2598746f9c
ahmedsayman03 <ahmed.s.ayman16@gmail.com>
2022-10-13 11:20:38 -0400

    Add code and doc for creating class and objs

---
+++ classes/class.py
@@ -0,0 +1,48 @@
+class MyClass:
+    """Classes have statements (normally function definitions)
+
+    A class has it's own namespace, local var assignments go to this namespace
+
+    when class definition is done (object created), local scope goes back to original scope with the class object
+    in the new local scope
+    """
+    i = 43
+
+    def __init__(self, x, y):
+        """To make objects have initial state, use this function
+
+        Obj creation / class instantiation invokes this automatically for newly created instance"""
+        self.data = []
+        self.x = x
+        self.y = y
+
+    def f(self):
+        """self is the instance object. obj.f is same as MyClass.f(obj)
+
+        Calling f method with arguments is = calling f with obj inserted as first argument"""
+        return 'hello world'
+
+    # obj.i
+    # obj.f  # a function object
+
+
+# Create class instance / obj
+mc = MyClass(2, 4)
+print(mc.x)
+print(mc.y)
+print(mc.i)
+
+
+# Instance objects only understand attribute names
+# attribute names: attributes, methods
+
+# Add attribute
+mc.test = 3
+print(mc.test)
+
+# method belongs to obj
+# class function objs are methods for instance obj
+mc.f  # method object, not a function objection
+
+# Classes (function objects)
+# instances (method objects)
