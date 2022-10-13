9b3f5b5d51bd0532c76cfeb22e84c746c293c8d2
ahmedsayman03 <ahmed.s.ayman16@gmail.com>
2022-10-13 10:16:21 -0400

    Add doc on classes

---
+++ classes/classes.py
@@ -0,0 +1,12 @@
+def classes():
+    """data, function = class
+
+    new class = new obj / instance
+    obj - attributes
+        - methods to modify state
+
+    Python class has std features of OOP:
+    inheritance (many base classes), override (methods) in derived class, method can call base class method
+
+    like module, created at run-time"""
+    pass
