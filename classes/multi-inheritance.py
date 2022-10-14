ef9ccce25c13584bc309b7d9e8a92f1c17e01006
ahmedsayman03 <ahmed.s.ayman16@gmail.com>
2022-10-14 11:21:22 -0400

    Add doc for multiple inheritance

---
+++ classes/multi-inheritance.py
@@ -0,0 +1,19 @@
+class A(B, C, D):
+    """
+    attribute search:
+    A
+    B
+    B2 (B2 is base class of B)
+    B3
+    not F (Parent class of B, that C and D share, e.g. Object - all classes extend object, object class parent base class)
+    C
+    C2
+    not F
+    D
+    not F
+
+    F parent shared blass for B C and D. This is called a diamond relationship.
+    Dynamic ordering solves this problem and doesn't repeat checking a same class that is a parent of multiple
+    inherited classes.
+    """
+    
