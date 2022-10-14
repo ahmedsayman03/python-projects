c58af8733f889576cf4c91cd18729c1d7bfe2141
ahmedsayman03 <ahmed.s.ayman16@gmail.com>
2022-10-14 13:30:39 -0400

    Add doc for generators (similar to class iterators)

--- classes/class_iterator_for_loop.py
+++ classes/class_iterator_for_loop.py
@@ -19,10 +19,9 @@ class Schools:
 
 
 s = Schools('spam')
-iter(s)
 
-for char in s:
-    print(char)
+for i in s:
+    print(i)
 
 
 """The way for loop works is it calls iter() in obj
@@ -34,3 +33,37 @@ it = iter(s)
 print(next(it))
 print(next(it))
 print(next(it))
+
+
+def re(data):
+    """Generators are like class based iterators but iter and next are created automatically.
+
+    yield something when they want to return it. when next() is called, it continues where it left off,
+    local vars and execution state is remembered.
+
+    raise StopIteration when terminating.
+
+    So you can use this in for loops."""
+    for char in range(len(data)-1, -1, -1):
+        yield data[char]
+
+
+for i in re('testing'):
+    print(i)
+
+
+def generator_expression():
+    """Like list comprehensions, we have generator comprehensions."""
+    g = (i*i for i in range(10))
+    print(g)  # generator
+    # Basically, g will yield for i, then another i, then another i, whenever next is called
+
+    # Useful if it's enclosed with a function
+    print(sum(g))
+
+    # valedictorian = max(((student.gpa, student.name) for student in graduates))
+    # Above is a generator that keeps giving (gpa, name) tuple seperated by comas (you can think of it like this)
+    # so the max function can use it.
+
+
+generator_expression()
