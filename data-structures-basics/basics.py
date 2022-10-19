d5d0b163a0a2e06d2e6846876709332d706ac905
ahmedsayman03 <ahmed.s.ayman16@gmail.com>
2022-10-19 10:59:43 -0400

    Add doc on basics of data strucures

---
+++ data-structures-basics/basics.py
@@ -0,0 +1,51 @@
+"""Data:
+        clear, understandable definition
+        single concept (atomic)
+        map to data element
+        unambiguous
+
+Data object: object with data
+
+Data TYPE: classify data
+e.g. int, string
+
+int = 1, 2, 3, 4... values
+string = 'abcd' etc ... values
+
+int: 1 + 1 = 2... operations!
+
+So,  data has values and operations
+
+Two types of data:
+1) Built in
+2) Derived
+
+Built-in
+    langauge supports
+    int, bool, . (decimal / float), str
+
+Derived
+    implementation in many ways, but still type of data
+
+    Combination of 1) built in data and 2) operations on them
+
+    e.g. list [1, 2, a, 3]
+    1, a is a combination of data
+
+    list.append()
+    ^ operation on them
+
+    Array, Stack, Queue
+
+
+Basic OPERATIONS:
+    DS -> data -> data processed by ops
+
+    choose DS by what ops need to frequently be used on the DS
+
+    traverse / travel through it
+    search
+    insert
+    delete
+    sort, rearrange
+    combine, merge"""
